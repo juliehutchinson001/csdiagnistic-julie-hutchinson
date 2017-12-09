eed7c0f29aa125916472e2d6e7c951ca368a0e1b
Julie Hutchinson <julie.hutchinson001@outlook.com>
2017-12-09 18:35:28 -0500

    explanation of the fizzbuzz algorithm

---
+++ fizzbuzz.py
@@ -0,0 +1,17 @@
+#Define a function called fizzbuzz
+#This function will receive two variables or inputs
+#Inside the fizzbuzz function, a conditional will be
+#created to meet the conditions as follows:
+# if a number is divisible by 5 and by 3, the function
+#will print the string "fizzbuzz"
+#if a number is only divisible by 3 (zero-remainder),
+#the function will print "fizz"
+#if a number is only divisible by 5, the function will
+#print "buzz"
+#else, print the number received.
+#Then the function will be invoked so that it accomplish
+#its main purpose: when given two inputs (numbers)
+#the function will print the sequence of numbers in 
+#between the two inputs as long as any of the if-conditions
+#are not met, when a condition is met, the function
+#will print the replacement output
