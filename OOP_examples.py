f8c1ce8b9338d759c0acc8611422b2b62f52b29f
Julie Hutchinson <julie.hutchinson001@outlook.com>
2017-12-09 19:18:52 -0500

    class Automobile defined

--- OOP_examples.py
+++ OOP_examples.py
@@ -1,15 +1,59 @@
-class Car:
+#class Car:
+#
+#def __init__(self, color):
+#self.num_wheels = 4
+#self.make = 'chrisler'
+#self.model = 'PT Cruiser'
+#self.color = color
+#self.speed = 100
+#
+#def sound(self):
+#print("Beep Beep!")
+#
+#def description(self):
+#print('{}:{}, {}:{}, {}:{}, {}:{}, {}:{}'.format('make', self.make, 'model', self.model, 'color', self.color, 'speed', self.speed, 'num_wheels', self.num_wheels))
 
-def __init__(self, color):
-self.num_wheels = 4
-self.make = 'chrisler'
-self.model = 'PT Cruiser'
-self.color = color
-self.speed = 100
+class Automobile(object):
+    def __init__(self, color):
+        self.color = color
+        self.model = "Honda"
 
-def honk(self):
-print("Beep Beep!")
+    def sound(self):
+        print "Bee Beep!"
 
-def description(self):
-print('{}:{}, {}:{}, {}:{}, {}:{}, {}:{}'.format('make', self.make, 'model', self.model, 'color', self.color, 'speed', self.speed, 'num_wheels', self.num_wheels))
+    def description(self):
+        print vars(self)
 
+class Motorcycle(Automobile):
+    def __init__(self, color):
+        Automobile.__init__(self, color)
+        self.wheelNum = 2
+        self.make = "Ducati?"
+        self.top_speed = 70
+
+class Car(Automobile):
+    def __init__(self, color):
+        Automobile.__init__(self, color)
+        self.wheelNum = 4
+        self.make = "Accord?"
+        self.top_speed = 100
+
+class SemiTruck(Automobile):
+    def __init__(self, color):
+        Automobile.__init__(self, color)
+        self.wheelNum = 16
+        self.make = "Big Rig?"
+        self.top_speed = 60
+
+if __name__ == "__main__":
+    test1 = Car("Blue")
+    print test1.color
+    print test1.top_speed
+    test1.sound()
+    test1.description()
+
+    test2 = Motorcycle("Black")
+    print test2.color
+    print test2.top_speed
+    test2.sound()
+    test2.description()
