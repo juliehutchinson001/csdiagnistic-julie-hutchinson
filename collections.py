e97f7a9ea9ba105b783cc62fb2266f2e7a45db53
Julie Hutchinson <julie.hutchinson001@outlook.com>
2017-12-09 19:10:06 -0500

    explanation of collection examples

---
+++ collections.py
@@ -0,0 +1,14 @@
+# List:
+my_list = [1, 2, 3, 4]
+my_list.append(5) # the append method adds a new value to the end of the list
+
+# Dictionary:
+my_dict = {'duane': 26, 'julie': 28, 'pepito': 30}
+
+my_dict['julie'] = 'pepita' # Now it is like this: {'julie':'pepita'}
+
+# Tuple:
+my_tuple = ('princess', 'lancelot', 'twinky')
+print(my_tuple[1]) # >> 'lancelot'
+
+
